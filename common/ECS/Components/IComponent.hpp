1f38a469111d982477adbdbbb0707beb26d03074
Samuel Bleau <samuel.bleau@epitech.eu>
2025-12-03 12:19:38 +0100

    feat: Added IComponents with Component id and type getting logic

--- common/ECS/Components/IComponent.hpp
+++ common/ECS/Components/IComponent.hpp
@@ -4,3 +4,34 @@
 ** File description:
 ** IComponent
 */
+
+#ifndef ICOMPONENT_HPP_
+#define ICOMPONENT_HPP_
+
+#include <cstddef>
+
+namespace ecs {
+    using ComponentType = std::size_t;
+
+    class IComponent {
+       public:
+        virtual ~IComponent() = default;
+
+        virtual ComponentType getType() const = 0;
+    };
+
+    // Unique ID generator for each component type
+    inline ComponentType getUniqueComponentType() {
+        static ComponentType lastID = 0;
+        return lastID++;
+    }
+
+    // Template to get the unique ID for a specific component type T
+    template <typename T>
+    ComponentType getComponentType() {
+        static ComponentType typeID = getUniqueComponentType();
+        return typeID;
+    }
+}
+
+#endif

f4731ecf36cfc8f0ec25e6238001c4bc5de029cd
Hugo Fabresse <hugo.fabresse@epitech.eu>
2025-12-06 16:33:56 +0100

    feat: add ThreadPool skeleton and docs

---
+++ server/Core/ThreadPool/ThreadPool.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** Created by hugo on 06/12/2025
+** File description:
+** ThreadPool.cpp
+*/
+
+#include "ThreadPool.hpp"
+#include <thread>
+
+namespace server {
+
+ThreadPool::ThreadPool(size_t threadCount)
+: _threadCount(threadCount)
+{
+}
+
+ThreadPool::~ThreadPool() {
+}
+
+void ThreadPool::enqueue(Task task) {
+    // Prototype only: implementation will be added later
+}
+
+void ThreadPool::start() {
+    // Prototype only: implementation will be added later
+}
+
+void ThreadPool::stop() {
+    // Prototype only: implementation will be added later
+}
+
+size_t ThreadPool::size() const {
+    return _threadCount;
+}
+
+} // namespace server
+

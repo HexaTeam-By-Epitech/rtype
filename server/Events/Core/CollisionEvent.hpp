cf43c61ed48c53692dd85d88e933af93f55070f4
Hugo Fabresse <hugo.fabresse@epitech.eu>
2025-12-06 18:02:59 +0100

    feat: add Events/Core skeleton and docs

---
+++ server/Events/Core/CollisionEvent.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** Created by hugo on 06/12/2025
+** File description:
+** CollisionEvent.hpp
+*/
+
+#pragma once
+
+#include "IEvent.hpp"
+
+namespace server {
+
+/**
+ * @class CollisionEvent
+ * @brief Represents a collision between two entities
+ */
+class CollisionEvent : public IEvent {
+public:
+    CollisionEvent() = default;
+    ~CollisionEvent() override = default;
+
+    /**
+     * @brief Get the first entity ID involved in the collision
+     */
+    int getEntityA() const;
+
+    /**
+     * @brief Get the second entity ID involved in the collision
+     */
+    int getEntityB() const;
+};
+
+} // namespace server
+

245b4097f27d6b0c3eb0b55ea83b0855d6952c38
Hugo Fabresse <hugo.fabresse@epitech.eu>
2025-12-06 18:30:18 +0100

    feat: add Game/World skeleton and docs

---
+++ server/Game/World/World.hpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2025
+** Created by hugo on 06/12/2025
+** File description:
+** World.hpp
+*/
+
+#pragma once
+
+#include "IWorld.hpp"
+#include <vector>
+
+namespace server {
+
+/**
+ * @class World
+ * @brief Concrete implementation of IWorld
+ */
+class World : public IWorld {
+public:
+    World() = default;
+    ~World() override = default;
+
+    // Prototypes
+    void addEntity(int entityID) override;
+    void removeEntity(int entityID) override;
+    void update(float dt) override;
+
+private:
+    std::vector<int> _entities;
+};
+
+} // namespace server
+

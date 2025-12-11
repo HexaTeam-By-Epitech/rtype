c83c72a6273ec3169d864d3ef19dffc6a14e15df
Swan Sarli <swan.sarli@epitech.eu>
2025-12-11 10:38:41 +0100

    fix: typo

--- client/Rendering/Rendering.cpp
+++ client/Rendering/Rendering.cpp
@@ -15,7 +15,6 @@ Rendering::~Rendering() {
     shutdown();
     _graphics.CloseWindow();
     _graphics.~RaylibGraphics();
-    r
 }
 
 bool Rendering::initialize(uint32_t width, uint32_t height, const std::string &title) {
